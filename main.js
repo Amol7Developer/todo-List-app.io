84958acff0b717c6baab58d24db1017cd08a5e0c
Amol7Developer <amol0783com@gmail.com>
2023-04-09 00:22:51 +0530

    code uploaded

--- main.js
+++ main.js
@@ -11,10 +11,27 @@ document.querySelector('#push').onclick = function(){
                     ('#newtask input').value}
                 </span>
                 <button class="delete">
-                <i class="far fa-trash-alt"></i>
+                <i class="fa-regular fa-trash-can"></i>
                 </button>
             </div>
         `;
+
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        var tasks = document.querySelectorAll(".tasks");
+        for(var i=0;i<tasks. length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed'); 
+            }
+        }
+        
+        document.querySelector("#newtask input").value = "";
     }
     
 }
